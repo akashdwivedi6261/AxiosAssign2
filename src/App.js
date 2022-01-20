86e51c9747bd704b2ba097025817e5799f3af276
akashdwivedi6261 <adarshsen6261@gmail.com>
2022-01-20 19:18:38 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from "react";
+import axios from "axios"
+import Input from "./Components/Input"
 function App() {
+ const [users, setUsers] = useState([])
+ const handleSearch = (query)=> {
+    axios.get(`https://api.github.com/search/users?q=${query}`).then((res)=> {
+      console.log(res.data.items)
+      setUsers(res.data.items)
+    })
+ }
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Input handleSearch={handleSearch}/>
     </div>
   );
 }
---
+++ src/Components/Input.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react"
+
+export default function Input({handleSearch}) {
+    const [query, setQuery] = useState("")
+    return(
+        <>
+        <input placeholder="Search users" value={query} onChange={()=>setQuery(e.target.value)}/>
+        <button onClick={()=>handleSearch(query)}>Search users</button>
+        </>
+    )
+}
