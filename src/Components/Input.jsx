0f6058e5a4d6f4b23a67f9c8b228c49396c19840
akashdwivedi6261 <adarshsen6261@gmail.com>
2022-01-20 19:24:11 +0530

    msg

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -4,7 +4,7 @@ export default function Input({handleSearch}) {
     const [query, setQuery] = useState("")
     return(
         <>
-        <input placeholder="Search users" value={query} onChange={()=>setQuery(e.target.value)}/>
+        <input placeholder="Search users" value={query} onChange={(e)=>setQuery(e.target.value)}/>
         <button onClick={()=>handleSearch(query)}>Search users</button>
         </>
     )
